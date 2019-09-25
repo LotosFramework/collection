a1a7e66d1ba75e07f5149125e6e4e654dbde268f
McLotos <mclotos@gmail.com>
2019-09-25 15:29:07 +0600

    init repository

---
+++ src/Collection.php
@@ -0,0 +1,159 @@
+<?php
+
+/*
+ * This file is part of the (c)Lotos framework.
+ *
+ * (c) McLotos <mclotos@xakep.ru>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace Lotos\Collection;
+
+use Ds\Sequence as SequenceInterface;
+
+class Collection extends Sequence
+{
+
+    public function __construct(array $item = null)
+    {
+        if(is_array($item) && (array_keys($item) !== range(0, count($item) - 1)) && count($item)>0) {
+            parent::__construct();
+            $this->push($item);
+        } else {
+            parent::__construct($item);
+        }
+    }
+
+    public function where(...$params) : SequenceInterface
+    {
+        if(count($params) == 3) {
+            list($prop, $symbol, $value) = $params;
+        } elseif(count($params) == 2) {
+            list($prop, $value) = $params;
+        }
+        $symbol = ($symbol) ?? '=';
+        $filtered = $this->filter(function($elem) use ($prop, $symbol, $value) {
+            if(is_array($elem)) {
+                return $this->getExpressionResult($elem[$prop], $symbol, $value);
+            } elseif(is_object($elem)) {
+                $method = 'get' . ucfirst($prop);
+                if(method_exists($elem, $method)) {
+                    return $this->getExpressionResult($elem->$method(), $symbol, $value);
+                } elseif(substr($prop, 0, 2) == 'is') {
+                    return $this->getExpressionResult($elem->$prop(), $symbol, $value);
+                }
+            }
+        })->toArray();
+        return $this->newInstance($filtered);
+    }
+
+    public function whereBetween(string $property, array $values) : SequenceInterface
+    {
+        return $this->newInstance($this->filter(function($elem) use ($property, $values) {
+            if($values[0] > $values[1]) {
+                return (($elem[$property] < $values[0]) && ($elem[$property] > $values[1]));
+            } elseif($values[0] < $values[1]) {
+                return (($elem[$property] < $values[1]) && ($elem[$property] > $values[0]));
+            }
+        })->toArray());
+    }
+
+    public function whereIn(string $property, array $values) : SequenceInterface
+    {
+        return $this->newInstance($this->filter(function($elem) use($property, $values) {
+            return (in_array($elem[$property], $values));
+        })->toArray());
+    }
+
+    private function getExpressionResult($prop, $symbol, $value) : bool
+    {
+        switch($symbol) {
+            case '>':
+                return ($prop > $value);
+            break;
+            case '<':
+                return ($prop < $value);
+            break;
+            case '=':
+                return ($prop == $value);
+            break;
+            case '>=':
+                return ($prop >= $value);
+            break;
+            case '<=':
+                return ($prop <= $value);
+            break;
+            case '<>':
+                return ($prop <> $value);
+            break;
+            case '!=':
+                return ($prop != $value);
+            break;
+        }
+    }
+
+    public function newInstance(array $data = []) : Collection
+    {
+        $class = get_called_class();
+        $clone = new $class;
+        if(count($data) > 0) {
+            foreach($data as $element) {
+                $clone->push($element);
+            }
+        }
+        return $clone;
+    }
+
+    public function whereNull($property) : SequenceInterface
+    {
+        return $this->newInstance($this->filter(function($elem) use ($property) {
+            if(is_array($elem)) {
+                return is_null($elem[$property]);
+            } elseif(is_object($elem)) {
+                $method = 'get' . ucfirst($property);
+                if(method_exists($elem, $method)) {
+                    return is_null($elem->$method());
+                } elseif(substr($property, 0, 2) == 'is') {
+                    return is_null($elem->$property());
+                }
+                return is_null($elem->$method());
+            }
+        })->toArray());
+    }
+
+    public function whereNotNull($property) : SequenceInterface
+    {
+        return $this->newInstance($this->filter(function($elem) use ($property) {
+            $method = 'get'.$property;
+            if(method_exists($elem, $method)) {
+                return !is_null($elem->$method());
+            } elseif(substr($property, 0, 2) == 'is') {
+                return !is_null($elem->$property());
+            }
+        })->toArray());
+    }
+
+    public function whereContain(string $param, string $value) : SequenceInterface
+    {
+        return $this->newInstance($this->filter(function($elem) use ($param, $value) {
+            $length = strlen($value);
+            $method = 'get'.ucfirst($param);
+            return (substr($elem->$method(), 0, $length) == substr($value, 0, $length));
+        })->toArray());
+    }
+
+    public function whereNotIn(string $property, array $values) :SequenceInterface
+    {
+        return $this->newInstance($this->filter(function($elem) use ($property, $values) {
+            if(is_array($elem) && (array_key_exists($property, $elem))) {
+                return (!in_array($elem[$property], $values));
+            } elseif(is_object($elem) && property_exists($elem, $property)) {
+                $method = 'get'.ucfirst($property);
+                return (!in_array($elem->$method(), $values));
+            }
+        })->toArray());
+    }
+
+}
---
+++ src/Sequence.php
@@ -0,0 +1,237 @@
+<?php
+
+/*
+ * This file is part of the (c)Lotos framework.
+ *
+ * (c) McLotos <mclotos@gmail.com>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace Lotos\Collection;
+
+use Ds\{
+    Sequence as SequenceInterface,
+    Collection as CollectionInterface,
+    Deque
+};
+use \UnderflowException;
+use \IteratorAggregate;
+
+class Sequence implements IteratorAggregate, SequenceInterface {
+
+    private $items;
+
+    public function __construct(array $items = null)
+    {
+        $this->items = (is_null($items))
+            ? null
+            : new Deque($items);
+    }
+
+    public function allocate(int $capacity) : void
+    {
+        $this->items->allocate($capacity);
+    }
+
+    public function apply(callable $callback) : void
+    {
+        $this->items->apply($callback);
+    }
+
+    public function capacity() : int
+    {
+        return $this->items->capacity();
+    }
+
+    public function contains(...$values) : bool
+    {
+        if($this->items) {
+            return call_user_func_array([$this->items, 'contains'], array_values($values));
+        } else {
+            return false;
+        }
+    }
+
+    public function filter(?callable $callback = null) : SequenceInterface
+    {
+        if($this->items) {
+            return $this->items->filter($callback);
+        }
+        return new self();
+    }
+
+    public function find($value)
+    {
+        if($this->items) {
+            return $this->items->find($value);
+        }
+        return null;
+    }
+
+    public function first()
+    {
+        if($this->items) {
+            return $this->items->first();
+        }
+        throw new UnderflowException('No items in collection');
+    }
+
+    public function get(int $index)
+    {
+        return $this->items->get($index);
+    }
+
+    public function insert(int $index, ...$values) : void
+    {
+        $params = array_values($values);
+        array_unshift($params, $index);
+        call_user_func_array([$this->items, 'insert'], array_values($params));
+    }
+
+    public function join(string $glue = null) : string
+    {
+        return $this->items->join($glue);
+    }
+
+    public function last()
+    {
+        if($this->items) {
+            return $this->items->last();
+        } else {
+            return null;
+        }
+    }
+
+    public function map(callable $callback) : SequenceInterface
+    {
+        if($this->items) {
+            return $this->items->map($callback);
+        }
+        throw new UnderflowException('Not items in collection');
+    }
+
+    public function merge($values) : SequenceInterface
+    {
+        return $this->items->merge($values);
+    }
+
+    public function pop()
+    {
+        return $this->items->pop();
+    }
+
+    public function push(...$values) : void
+    {
+        if($this->items) {
+            call_user_func_array([$this->items, 'push'], array_values($values));
+        } else {
+            $this->items = new Deque();
+            call_user_func_array([$this->items, 'push'], array_values($values));
+        }
+    }
+
+    public function reduce(callable $callback, $initial = null)
+    {
+        return $this->items->reduce($callback, $initial);
+    }
+
+    public function remove(int $index)
+    {
+        return $this->items->remove($index);
+    }
+
+    public function reverse() : void
+    {
+        $this->items->reverse();
+    }
+
+    public function reversed() : SequenceInterface
+    {
+        return $this->items->reversed();
+    }
+
+    public function rotate(int $rotations) : void
+    {
+        $this->items->rotate($rotations);
+    }
+
+    public function set(int $index, $value) : void
+    {
+        $this->items->set($index, $value);
+    }
+
+    public function shift()
+    {
+        return $this->items->shift();
+    }
+
+    public function slice(int $index, ?int $length = null ) : SequenceInterface
+    {
+        return $this->items->slice($index, $length);
+    }
+
+    public function sort(?callable $comparator = null) : void
+    {
+        $this->items->sort($comparator);
+    }
+
+    public function sorted(callable $comparator) : SequenceInterface
+    {
+        return $this->items->sorted($comparator);
+    }
+
+    public function sum() : int
+    {
+        return $this->items->sum();
+    }
+
+    public function unshift(...$values) : void
+    {
+        call_user_func_array([$this->items, 'unshift'], array_values($values));
+    }
+
+    public function getIterator() : ArrayIterator
+    {
+        return new ArrayIterator($this);
+    }
+
+    public function clear() : SequenceInterface
+    {
+        return $this->items->clear();
+    }
+
+    public function copy() : CollectionInterface
+    {
+        return $this->items->copy();
+    }
+
+    public function toArray() : array
+    {
+        if($this->items) {
+            return $this->items->toArray();
+        } else {
+            return [];
+        }
+    }
+
+    public function isEmpty() : bool
+    {
+        return $this->items->isEmpty();
+    }
+
+    public function count() : int
+    {
+        if($this->items) {
+            return $this->items->count();
+        } else {
+            return 0;
+        }
+    }
+
+    public function jsonSerialize()
+    {
+        return $this->items->jsonSerialize();
+    }
+}
